fe69cc1d692712c9208d7eee63709c53afc43802
alba <arga.alba@gmail.com>
2021-06-07 12:10:15 -0300

    desafio 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import  {ItemListContainer}  from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      
-      <NavBar />
-      <h1> Home </h1>
-
+      <header className="App-header" style={ {background: '#1211'} }>
+        <NavBar/>
+         </header>
+         <ItemListContainer greeting='Bienvenidos a nuestra tienda!' />
 
     </div>
   );
