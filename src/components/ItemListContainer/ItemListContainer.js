56325c3b86fa7961d6c94565c729ebad562376d9
alba <arga.alba@gmail.com>
2021-07-01 18:01:40 -0300

    routing pagina

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,37 +1,9 @@
 import React,{ useState, useEffect} from 'react';
 import ItemList from '../ItemList/ItemList';
-import img1 from '../../assets/img/img1.png';
-import img2 from '../../assets/img/img2.png';
-import img6 from '../../assets/img/img6.png';
 
 const ItemListContainer = () => {
   
   const [data, setData] = useState([]);
-
- const productos = [
-  
- {
-   img:img1,
-   tittle: 'Reloj Clara',
-   description: 'Modelo Clásico',
-   price: '$2000'
-
-  },{
-    img:img2,
-    tittle: 'Reloj Francia',
-   description: 'Malla regulable',
-   price: '$1500'
-
-  },{
-  img:img6,
-  tittle: 'Reloj Imantado',
- description: 'Modelo unisex',
- price: '$3500'
-
- }
-
- ];
-
   const task=  new Promise((resolve) => {
       setTimeout(() => {
         resolve(productos);
@@ -40,24 +12,24 @@ const ItemListContainer = () => {
      });
    
      useEffect (()=> {
-      task
-      .then((info)=> 
-        {setData(info) }).catch(e=> 
-          console.log(e));
-        
-     },[]);
-
-  return(
-      <>
-     
+      fetch('https://my-json-server.typicode.com/albaarg/tienda/blob/main/db.json')
+      .then(response=> response.json()) 
+      .then((info) => {
+      setData(info)
+    }).catch(e=> {
+          console.log(e);
+      });  
+     });
+  
+           return(
       
-      <ItemList productos ={data} />
-      </>
+      <ItemList info ={data} />
+      
       
     );
 
+    },
+    
 
-
-}
 
 export default ItemListContainer;
