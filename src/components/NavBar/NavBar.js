510fd2062604b41c1c067c3c359f7974a337c764
alba <arga.alba@gmail.com>
2021-07-02 23:38:55 -0300

    arreglos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,15 +3,13 @@ import '../NavBar/NavBar';
 import CartWidget from '../CartWidget/CartWidget';
 import logo from '../../assets/img/b.r.png';
 import {NavLink} from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import {Button} from 'react-bootstrap';
+import {Button, Nav, NavDropdown} from 'react-bootstrap';
 
 
 
 const NavBar = () => {
       return  ( 
-          
-        <div className='d-flex align-items-center justify-content-between p-2 bg-light'> 
+             <div className='d-flex align-items-center justify-content-between p-2 bg-light'>
              <NavLink to={'/'}  className="container-logo">
              <img src={logo} className="imgLogo"/>
              </NavLink>
@@ -20,27 +18,19 @@ const NavBar = () => {
                  <NavLink to={'/category'} className="container-inicio">
                  <Button className="btn btn-light">Inicio</Button>
                  </NavLink>
-                  <NavLink to={'/category'} className="dropdown-category"> 
-                 <div className="dropdown">
-                 <button className="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">         
-                  Catálogo
-                 </button>
-                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                 <NavLink to={'/category/:hombre'} className="dropdown-item" href="#">Hombre</NavLink>		
-                 <NavLink to={'/category/:mujer'} className="dropdown-item" href="#">Mujer</NavLink>		
-                 <NavLink to={'/category/:unisex'} className="dropdown-item" href="#">Unisex</NavLink>
-                 </div>
-                 </div>
-                 </NavLink>
+                 <NavDropdown title="Catalogo"  classname="dropdown-menu ">
+                 <NavLink to={'/category/:hombre'} className="dropdown-item" component={NavDropdown.Item}>Hombre</NavLink>		
+                 <NavLink to={'/category/:mujer'} className="dropdown-item" component={NavDropdown.Item}>Mujer</NavLink>		
+                 <NavLink to={'/category/:unisex'} className="dropdown-item" component={NavDropdown.Item}>Unisex</NavLink>
+                 </NavDropdown>
                  <NavLink to={'/contacto'} className="container-contacto">
                  <Button className="btn btn-light">Contacto</Button>
                  </NavLink>
                  </Nav>
             </ul>
-            <CartWidget/>
-            
-          </div>
-        
+            <CartWidget/>    
+            </div>  
+
 
       )   
 }
