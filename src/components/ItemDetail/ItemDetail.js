3aca27c80ffa0d888117a652a4fabc93dee556e6
alba <arga.alba@gmail.com>
2021-07-09 23:29:05 -0300

    eventos

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ItemCount from '../ItemCount/ItemCount';
+import {Button} from 'react-bootstrap';
 
-const onAdd= () => {}
+const [finish, setFinish]= useState(false)
+const onAdd= (items) => {
+  setFinish(true)
+}
 const ItemDetail = (prop) => {
 const {img, tittle, description, price, category, colour} = prop
 return(
     
-       <div className="producto-info">
-            <div className="artc-img-item">
-                <img src={img} />
-             </div>
-             <div className="item-info-producto">
-                <h4>
-                  <a href="/">{tittle}</a>
-                </h4>
-                <div>{description}</div>
-                <div className="producto_precio">
-                    <div className="grid-precio">
-                        <span className="money">{price}</span>
-                             </div>
-                    <div className="producto_categoria" >
-                    <div>{category}</div>
-                    </div>
-                    <div className="producto_color" >
-                    <div> {colour}</div>
-                    <ItemCount initial={1} stock={20} onAdd={onAdd}/> 
-                    </div>
-                </div>
-                </div>         
+      <div className="producto-info">
+        <div className="artc-img-item">
+          <img src={img} />
         </div>
+      <div className="item-info-producto">
+         <h4>
+         <a href="/">{tittle}</a>
+         </h4>
+         <div>{description}</div>
+      <div className="producto_precio">
+        <div className="grid-precio">
+        <span className="money">{price}</span>
+        </div>
+      <div className="producto_categoria" >
+        <div>{category}</div>
+        </div>
+       <div className="producto_color" >
+        <div> {colour}</div>
+         {finish ? <Button variant="btn btn-light">Finalizar Compra</Button> :
+         <ItemCount initial={1} stock={20} onAdd={onAdd}/>
+        }                                    
+        </div>
+       </div>
+       </div>         
+      </div>
     
 )
 
