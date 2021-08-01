2c818bcdbceed3d4e60cd4483530727f68730418
Oscar Cornejo <oscarcornejo10@gmail.com>
2021-08-01 19:37:25 -0400

    FIX para CartContext.js, CartList.js, Checkout.js, ItemListContainer.js, se ordena el código para CartProvider.js y se alinean las Cards

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,8 +7,7 @@ import { Card } from "react-bootstrap";
 
 // Muestra todo el detalle de los productos
 const Item = (prop) => {
-  const { id, imageId, tittle, description, categoryId, price, stock } =
-    prop.element;
+  const { id, imageId, tittle, description, categoryId, price, stock } = prop.element;
   const [finish, setFinish] = useState(true);
 
   const _handleAdd = (cantidad) => () => {
@@ -18,37 +17,32 @@ const Item = (prop) => {
 
   return (
     <>
-      <div
-        className="d-flex justify-content-row"
-        style={{ marginBottom: "12rem" }}
-      >
-        <div className="col-md-4">
-          <Card>
-            <Card.Img src={imageId} />
-            <Card.Body>
-              <Card.Title className="card-tittle">{tittle.toUpperCase()}</Card.Title>
-              <Card.Text className="card-description">{description}</Card.Text>
-              <Card.Text className="card-category">{categoryId}</Card.Text>
-              <Card.Text className="card-price">${price} </Card.Text>
-              <Card.Text className="card-stock">En stock:{stock} </Card.Text>
-              <Link to={`/item/${id}`}>
-                {" "}
-                <Button className="btn btn-light">Ver detalle</Button>{" "}
-              </Link>
-            </Card.Body>
-          </Card>
-          {finish ? (
-            <ItemCount initial={1} stock={20} onAdd={_handleAdd} />
-          ) : (
-            <Link to={"/Cart"}>
+      <div className="col-md-4">
+        <Card>
+          <Card.Img src={imageId} />
+          <Card.Body>
+            <Card.Title className="card-tittle">{tittle.toUpperCase()}</Card.Title>
+            <Card.Text className="card-description">{description}</Card.Text>
+            <Card.Text className="card-category">{categoryId}</Card.Text>
+            <Card.Text className="card-price">${price} </Card.Text>
+            <Card.Text className="card-stock">En stock:{stock} </Card.Text>
+            <Link to={`/item/${id}`}>
               {" "}
-              <div className="d-flex justify-content-center align-items-baseline">
-                {" "}
-                <Button variant="secondary">Finalizar Compra</Button>
-              </div>{" "}
+              <Button className="btn btn-light">Ver detalle</Button>{" "}
             </Link>
-          )}
-        </div>
+          </Card.Body>
+        </Card>
+        {finish ? (
+          <ItemCount initial={1} stock={20} onAdd={_handleAdd} />
+        ) : (
+          <Link to={"/Cart"}>
+            {" "}
+            <div className="d-flex justify-content-center align-items-baseline">
+              {" "}
+              <Button variant="secondary">Finalizar Compra</Button>
+            </div>{" "}
+          </Link>
+        )}
       </div>
     </>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -52,19 +52,21 @@ const ItemListContainer = () => {
   }, [categoryId]);
 
   return (
-    <>
-      <div>
+    <div className="container mb-5">
+      <div className="d-flex justify-content-between">
         {loading ? (
           <div className="loader text-center" style={{ marginTop: "20%", height: "100vh" }}>
             <Spinner animation="border" variant="dark" />{" "}
           </div>
         ) : (
-          productlist.map((item) => (
-            <Item key={item.id} element={item} onAddCart={_handleAddCart(item)} />
-          ))
+          <div className="row">
+            {productlist.map((item) => (
+              <Item key={item.id} element={item} onAddCart={_handleAddCart(item)} />
+            ))}
+          </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
