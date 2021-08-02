3a5a0cc079f698a25cd4e0fed5cdcdd173667de4
alba <arga.alba@gmail.com>
2021-08-01 22:15:02 -0300

    entrega final

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -2,8 +2,6 @@ import { useContext, createContext } from "react";
 const CartContext = createContext({});
 export const useCartContext = () => useContext(CartContext);
 
-<<<<<<< HEAD
 export default CartContext;
-=======
 export default CartContext;
->>>>>>> 44cec6b383354baafb2784a9529ecfead69ce1de
+
