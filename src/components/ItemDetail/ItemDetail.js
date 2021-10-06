a389a7d34d9c56af39a713db32462f9f46a32fc4
alba <arga.alba@gmail.com>
2021-10-06 20:07:54 -0300

    styles

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,16 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CartContext from "../../Context/CartContext";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const ItemDetail = (prop) => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   const { imageId, tittle, description, price, categoryId, colour, stock } =
     prop;
   const [finish, setFinish] = useState(true);
@@ -18,37 +24,39 @@ const ItemDetail = (prop) => {
 
   return (
     <div>
-      <h4 class="p-3 m-4 ">{tittle}</h4>
-      <img
-        class=" object-center md:object-top ..."
-        src={imageId}
-        alt="img-item"
-      />
-      <div class="bg-indigo-100 md:bg-bg-indigo-200 rounded md:rounded-lg ... p-3 m-3">
-        <p class="text-purple-700 text-sm uppercase text-left md:text-center ...">
-          {description}
-        </p>
-        <p class="text-purple-700 text-sm uppercase text-left md:text-center ...">
-          {categoryId}
-        </p>
-        <p class="text-purple-700 text-sm uppercase text-left md:text-center ...">
-          ${price}
-        </p>
-        <p class="text-purple-700 text-sm uppercase text-left md:text-center ...">
-          {colour}
-        </p>
-        <p class="text-purple-700 text-sm uppercase text-left md:text-center ...">
-          {stock} En stock!{" "}
-        </p>
-      </div>
+      <div data-aos="fade-down">
+        <h4 class="p-3 m-4 ">{tittle}</h4>
+        <img
+          class=" object-center md:object-top ..."
+          src={imageId}
+          alt="img-item"
+        />
+        <div class="bg-gray-100 md:bg-bg-indigo-200 rounded md:rounded-lg ... p-3 m-3">
+          <p class="text-gray-400 text-sm uppercase text-left md:text-center ...">
+            {description}
+          </p>
+          <p class="text-gray-400 text-sm uppercase text-left md:text-center ...">
+            {categoryId}
+          </p>
+          <p class="text-gray-400  text-sm uppercase text-left md:text-center ...">
+            ${price}
+          </p>
+          <p class="text-gray-400  text-sm uppercase text-left md:text-center ...">
+            {colour}
+          </p>
+          <p class="text-gray-400  text-sm uppercase text-left md:text-center ...">
+            {stock} En stock!{" "}
+          </p>
+        </div>
 
-      {finish ? (
-        <ItemCount initial={1} stock={20} onAdd={onAdd} />
-      ) : (
-        <Link to={"/Cart"}>
-          <Button variant="secondary">Finalizar Compra</Button>
-        </Link>
-      )}
+        {finish ? (
+          <ItemCount initial={1} stock={20} onAdd={onAdd} />
+        ) : (
+          <Link to={"/Cart"}>
+            <Button variant="secondary">Finalizar Compra</Button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,39 +6,45 @@ import HomeIcon from "@material-ui/icons/Home";
 
 const NavBar = () => {
   return (
-    <div className="d-flex align-items-center justify-content-between py-5 bg-light">
-      <h1 className="m-6 animate-bounce w-10 h-10 ">BK Relojes</h1>
-      <nav className="flex items-center m-2 space-x-24 ">
-        <NavLink
-          to={"/"}
-          className="uppercase  hover:text-purple-400 md:no-underline... text-black"
-        >
-          <HomeIcon />
-        </NavLink>
+    <div className="topnav">
+      <div className="d-flex align-items-center justify-content-between py-5 bg-light">
+        <h1 className="m-6 animate-bounce w-10 h-10 ">
+          <strong>BK Relojes</strong>
+        </h1>
+        <nav className="w-full block flex-grow lg:flex lg:items-centers lg:w-auto text-center">
+          <div className="text-sm lg:flex-grow">
+            <NavLink
+              to={"/"}
+              className="block mt-4 lg:inline-block lg:mt-0 uppercase text-teal-200 text-black hover:text-purple-400 md:no-underline... mr-4 "
+            >
+              <HomeIcon />
+            </NavLink>
 
-        <NavLink
-          exact
-          to={"/categoria/Modelo Hombre"}
-          className="uppercase  hover:text-purple-400 md:no-underline... text-black "
-        >
-          Hombre
-        </NavLink>
-        <NavLink
-          exact
-          to={"/categoria/Modelo Mujer"}
-          className="uppercase hover:text-purple-400 md:no-underline text-black"
-        >
-          Mujer
-        </NavLink>
-        <NavLink
-          exact
-          to={"/categoria/Modelo Unisex"}
-          className="uppercase  hover:text-purple-400 md:no-underline... text-black"
-        >
-          Unisex
-        </NavLink>
-      </nav>
-      <CartWidget />
+            <NavLink
+              exact
+              to={"/categoria/Modelo Hombre"}
+              className="block mt-4 lg:inline-block lg:mt-0 uppercase text-teal-200 text-black hover:text-purple-400 md:no-underline... mr-4 "
+            >
+              Hombre
+            </NavLink>
+            <NavLink
+              exact
+              to={"/categoria/Modelo Mujer"}
+              className="block mt-4 lg:inline-block lg:mt-0 uppercase text-teal-200 text-black hover:text-purple-400 md:no-underline... mr-4"
+            >
+              Mujer
+            </NavLink>
+            <NavLink
+              exact
+              to={"/categoria/Modelo Unisex"}
+              className="block mt-4 lg:inline-block lg:mt-0 uppercase text-teal-200 text-black hover:text-purple-400 md:no-underline... mr-4"
+            >
+              Unisex
+            </NavLink>
+          </div>
+        </nav>
+        <CartWidget />
+      </div>
     </div>
   );
 };
